7be73142c8c4a2880f91c02891c5845ea12fafef
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-31 21:10:31 +0900

    Add files via upload

--- home.js
+++ home.js
@@ -4,7 +4,26 @@ $(function() {
     $('#cup').fadeIn();
   });
 
+  // ここからメニュー
+  $('#bar').click(function(){
+    $('#menu').slideDown();
+  });
 
+  $('#bar').click(function(){
+    $(this).fadeOut();
+  });
+
+  $('#batsu').click(function(){
+    $('#menu').slideUp();
+  });
+
+
+
+  $('#batsu').click(function(){
+    $('#bar').fadeIn();
+  });
+
+// ここまでメニュー
 
 
 });
